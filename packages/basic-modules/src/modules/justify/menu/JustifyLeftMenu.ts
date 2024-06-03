e688701c42317dcf7b0b8e1613d743c7a05dcafe
yaolongfei <2991205548@qq.com>
2024-06-03 18:23:14 +0800

    perf(issue 5259): video pos

--- packages/basic-modules/src/modules/justify/menu/JustifyLeftMenu.ts
+++ packages/basic-modules/src/modules/justify/menu/JustifyLeftMenu.ts
@@ -3,8 +3,8 @@
  * @author wangfupeng
  */
 
-import { Transforms, Element } from 'slate'
-import { IDomEditor, t } from '@wangeditor-next/core'
+import { Transforms, Editor, Element } from 'slate'
+import { IDomEditor, DomEditor, t } from '@wangeditor-next/core'
 import BaseMenu from './BaseMenu'
 import { JUSTIFY_LEFT_SVG } from '../../../constants/icon-svg'
 
@@ -13,6 +13,15 @@ class JustifyLeftMenu extends BaseMenu {
   readonly iconSvg = JUSTIFY_LEFT_SVG
 
   exec(editor: IDomEditor, value: string | boolean): void {
+    const { selection } = editor
+    console.log('selection', selection)
+    const tables = Editor.nodes(editor, {
+      at: selection,
+      match: n => {
+        const type = DomEditor.getNodeType(n)
+        return type === 'table'
+      },
+    })
     Transforms.setNodes(
       editor,
       {
